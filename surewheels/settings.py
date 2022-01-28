577bf0c0d732b146f971c27213daceee9b1e6645
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2022-01-29 00:28:17 +0100

    Update settings.py

--- surewheels/settings.py
+++ surewheels/settings.py
@@ -27,7 +27,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 SECRET_KEY = 'django-insecure-5x)$b4(rc)f*4)6$-x@mw)#jvb90m!@x2o(dh2+-4l=8v(-uo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','surewheel.herokuapp.com','www.surewheel.herokuapp.com','surewheelsng.com','wwww.surewheelsng.com']
 
